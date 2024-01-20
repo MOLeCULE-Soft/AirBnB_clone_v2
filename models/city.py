a8731209d77423d126bc8edde42976b1b3868d06
MOLeCULE-Soft <felixkimbu1@gmail.com>
2024-01-20 21:14:34 +0100

    Some cleanup4

--- models/city.py
+++ models/city.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
-"""This is the city class"""
+"""City class"""
+
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
-from sqlalchemy import ForeignKey
+from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
-    """This is the class for City
-    Attributes:
-        state_id: The state id
-        name: input name
-    """
+    """City class Implementation"""
+
     __tablename__ = "cities"
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
