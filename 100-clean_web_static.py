eca7300bc4b05ea7981fff0c6247b781e9fcf1a6
Tundroid <felixkimbu@outlook.com>
2024-02-02 01:31:26 +0100

    Task 5

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-# Fabfile to delete out-of-date archives.
+# Fabfile to delete out-of-date archives
 import os
 from fabric.api import *
 
-env.hosts = ["104.196.168.90", "35.196.46.172"]
+env.hosts = ["35.175.132.210", "35.153.232.176"]
 
 
 def do_clean(number=0):
