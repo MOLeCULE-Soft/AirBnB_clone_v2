834ab09e33c03bdc963b4cbb684e8d8fff57334d
Tundroid <felixkimbu@outlook.com>
2024-04-21 10:41:52 +0100

    Task 3

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -21,8 +21,7 @@ def c(text):
     return 'C {}'.format(escape(text).replace('_', ' '))
 
 
-@app.route('/python/<text>', defaults={'text', 'is cool'},
-           strict_slashes=False)
+@app.route('/python/', defaults={'text', 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python(text):
     return 'Python {}'.format(escape(text).replace('_', ' '))
