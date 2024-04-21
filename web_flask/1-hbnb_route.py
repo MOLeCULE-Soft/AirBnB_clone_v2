3d36d2414eb6556a9fffd50b7edc2215a56b22ff
Tundroid <felixkimbu@outlook.com>
2024-04-21 10:26:37 +0100

    Task 1

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -9,6 +9,10 @@ app = Flask(__name__)
 def root():
     return 'Hello HBNB!'
 
+@app.route('/', strict_slashes=False)
+def hbnb():
+    return 'HBNB'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
