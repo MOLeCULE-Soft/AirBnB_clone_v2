a2fbf099a3536cd8969de34c97b2b2e6b1875722
Tundroid <felixkimbu@outlook.com>
2024-04-21 11:23:37 +0100

    Task 5

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Hello Flask"""
-from flask import Flask, render_template
+from flask import Flask, render_template, abort
 from werkzeug.utils import escape
 
 app = Flask(__name__)
